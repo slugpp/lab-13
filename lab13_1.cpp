be17abc1d8f83324fb62f5f7e4f3042c5dbb3c46
fear <corefea@gmail.com>
2023-01-10 15:19:35 +0700

    13.1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
+
+long long int fibonacci(int x);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(int x){
+    if(x==0){
+        return 0;
+    }
+    else if(x==1){
+        return 1;
+    }
+    else{
+           return  fibonacci(x-1) + fibonacci(x-2);
+    }
 }
