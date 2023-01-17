bfe276e285f0686f9b7c4b9f34daae4cb27785d6
fear <corefea@gmail.com>
2023-01-17 22:17:26 +0700

    completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int a,int b);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int a,int b)
+{   
+    if(a%b==0)
+    {
+        return b;
+    }
+    else return gcd(b,a%b);
 }
